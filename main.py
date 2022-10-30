89a1b6fa6a9b4a773a7614623b1a25b566bdf983
just-grisha <67802158+just-grisha@users.noreply.github.com>
2022-10-30 08:10:03 +0300

    4 commit

--- main.py
+++ main.py
@@ -1,27 +1,5 @@
 import telebot
+import Bot
 
-TOKEN = '5742628016:AAGf8TffSFrJQGlJKMPjDF0gNEDADPUatJY'
+Bot.Bot_()
 
-bot = telebot.TeleBot(TOKEN)
-
-'''
-@bot.message_handler(commands=['start'])
-def start(message):
-    mes = f"Привет, {message.from_user.fisrt_name} {message.from_user.last_name}"
-    bot.send_message(message.chat.id, mes, parse_mode=None)
-
-
-@bot.message_handler()
-def say_hello_to_user(message):
-    if message.text == "Lol":
-        bot.send_message(message.chat.id, f"Привет,{message.first_name}", parse_mode=None)
-
-'''
-
-@bot.message_handler(commands=['start', 'help'])
-def send_welcome(message):
-	bot.reply_to(message, "Howdy, how are you doing?")
-
-
-bot.polling()
-while True:
